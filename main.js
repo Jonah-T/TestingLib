9446ca2d70973a924f34771f7a88172d226d159a
Jonah-T <66018841+Jonah-T@users.noreply.github.com>
2020-06-04 10:49:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -116,6 +116,16 @@
             google.charts.load('current', {'packages':['corechart']});
             google.charts.setOnLoadCallback(drawChart);
         function drawChart() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+              ['Mushrooms', 3],
+              ['Onions', 1],
+              ['Olives', 1],
+              ['Zucchini', 1],
+              ['Pepperoni', 2]
+            ]);
         }
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
