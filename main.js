a82f76f66873dd5da7fb5d8ef05aaa7648ab32ff
Jonah-T <66018841+Jonah-T@users.noreply.github.com>
2020-06-04 09:35:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -28,6 +28,7 @@
               },
               async: true
             });
+            google.charts.load('current', {'packages':['corechart']});
         }
 
 
@@ -73,7 +74,7 @@
             //            alert(e);
             //    }
             //}
-           // this.redraw();
+            this.redraw();
         //}
         }
     //loadScript(src, shadowRoot) {
@@ -108,7 +109,27 @@
  
 
 
-        redraw(){}
+        redraw(){
+            var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+                }
     });
 }
 //,
