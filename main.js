87af14af2e14488febe85edf7f6def2e32b481b5
Jonah-T <66018841+Jonah-T@users.noreply.github.com>
2020-06-04 10:19:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -114,7 +114,15 @@
 
         redraw(){
             google.charts.load('current', {'packages':['corechart']});
-            var data = new google.visualization.DataTable();
+            google.charts.setOnLoadCallback(drawChart);
+        }
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
         data.addColumn('string', 'Topping');
         data.addColumn('number', 'Slices');
         data.addRows([
@@ -133,7 +141,8 @@
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
-                }
+      }
+                
     });
 }
 //,
