627e38466d343a2f5040312f4c5df78cbd535e54
Jonah-T <66018841+Jonah-T@users.noreply.github.com>
2020-06-09 18:07:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,45 +15,28 @@
 
     customElements.define('com-sap-sample-testinglib', class TestingLib extends HTMLElement {
 
- 
-
- 
-
-            constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({mode: "open"});
-                    this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = 0;
+    constructor() {
+		super();	
+		this._shadowRoot = this.attachShadow({mode: "open"});
+		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+        this._firstConnection = 0;
            
-            let GoogleSRC = "https://www.gstatic.com/charts/loader.js";
-            $.ajax({
+        let GoogleSRC = "https://www.gstatic.com/charts/loader.js";
+        $.ajax({
               url: GoogleSRC,
               dataType: "script",
               success: function(data) {
-                console.log("Script loaded");
+              console.log("Script loaded");
               },
               async: false
             });
-            	var createDiv = document.createElement("div");
-		createDiv.setAttribute("id", "div_test");
-		createDiv.style.backgroundColor = "lightblue";
-		createDiv.style.width='250px';
-		createDiv.style.height='300px';
-
-
-		document.getElementsByTagName('body')[0].appendChild(createDiv);
+			
         }
-
-
- 
-
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this.redraw();
         }
-
- 
 
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -61,13 +44,9 @@
         
         }
 
- 
-
 
          //When the custom widget is updated, the Custom Widget SDK framework executes this function first
         onCustomWidgetBeforeUpdate(oChangedProperties) {
-
- 
 
 
         }
@@ -77,34 +56,9 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
         onCustomWidgetAfterUpdate(oChangedProperties) {
-            //var that = this;
-            //if (this._firstConnection === 0) {
-            //    this._firstConnection = 1;
-            //    let GoogleSRC = "https://www.gstatic.com/charts/loader.js";
-            //    async function LoadLibs() {
-            //        try {
-            //            await loadScript(GoogleSRC, _shadowRoot);
-            //        } catch (e) {
-            //            alert(e);
-            //    }
-            //}
-            this.redraw();
-        //}
+            //this.redraw();
         }
-    //loadScript(src, shadowRoot) {
-    //        return new Promise(function(resolve, reject) {
-    //            let script = document.createElement('script');
-    //            script.src = src;
-//
-    //            script.onload = () => {
-    //                console.log("Load: " + src);
-    //                resolve(script);
-    //            }
-    //            script.onerror = () => reject(new Error(`Script load error for ${src}`));
-//
-    //            shadowRoot.appendChild(script)
-    //        });
-    //}
+
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
         }
@@ -114,43 +68,40 @@
 
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
-        
+        // Commented out by default        
         onCustomWidgetResize(width, height){
             this.redraw();
         }
        
 
- 
-
-
         redraw(){
 
-            google.charts.load('current', {'packages':['corechart']});
-               google.charts.setOnLoadCallback(function () {
+			google.charts.load('current', {'packages':['corechart']});
+			google.charts.setOnLoadCallback(function () {
 						     drawChart(this._shadowRoot );
 						   });
-        function drawChart(Shadow) {
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-              ['Mushrooms', 3],
-              ['Onions', 1],
-              ['Olives', 1],
-              ['Zucchini', 1],
-              ['Pepperoni', 2]
-            ]);
-            var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
+						   
+			function drawChart(Shadow) {
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Topping');
+				data.addColumn('number', 'Slices');
+				data.addRows([
+				  ['Mushrooms', 3],
+				  ['Onions', 1],
+				  ['Olives', 1],
+				  ['Zucchini', 1],
+				  ['Pepperoni', 2]
+				]);
+				var options = {'title':'How Much Pizza I Ate Last Night',
+						   'width':400,
+						   'height':300};
 
-        // Instantiate and draw our chart, passing in some options.
-	   // var div = $('#chart_div');
-		var div = document.getElementById('div_test');  
-		 
-            var chart = new google.visualization.PieChart(div);
-            chart.draw(data, options);
+			// Instantiate and draw our chart, passing in some options.
+		   // var div = $('#chart_div');
+			var div = document.getElementById('div_test');  
+			 
+				var chart = new google.visualization.PieChart(div);
+				chart.draw(data, options);
 
         }
       // Callback that creates and populates a data table,
@@ -172,7 +123,7 @@
 
         // Set chart options
         var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
+                       'width':400,
                        'height':300};
 
         // Instantiate and draw our chart, passing in some options.
